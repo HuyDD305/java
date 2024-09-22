4088513cbd70a3f06eadc7af1174e03b6e842864
HuyDD305 <bribribridu@gmail.com>
2024-09-23 05:52:26 +0700

    h

--- Lab211/src/P0056/SalaryStatus.java
+++ Lab211/src/P0056/SalaryStatus.java
@@ -10,6 +10,11 @@ package P0056;
  */
 public class SalaryStatus extends Worker{
     private String status;
+
+    public SalaryStatus(String status, String name, int id, int age, double salary, String workLocation) {
+        super(name, id, age, salary, workLocation);
+        this.status = status;
+    }
     
     
     
